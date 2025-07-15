9491f605fabddc78e70f7fdcd48e4a6a06936bb2
Sourabh kakani <kakanisourabh23@gmail.com>
2025-07-15 18:52:25 +0530

    use of populate in emp

--- backend/src/controllers/employee.controller.js
+++ backend/src/controllers/employee.controller.js
@@ -37,8 +37,8 @@ const createEmployee = asyncHandler(async (req, res) => {
   console.log("Emp det ---", emp);
 
   const createdEmployee = await Employee.findById(emp._id)
-  .populate({ path: "department", select: "name description" })
-  .populate({ path: "role", select: "name description" });
+    .populate({ path: "department", select: "name description" })
+    .populate({ path: "role", select: "name description" });
 
   if (!createdEmployee) {
     throw new ApiError(500, "Employee not created successfully");
@@ -51,84 +51,96 @@ const createEmployee = asyncHandler(async (req, res) => {
     );
 });
 
-const listEmployees = asyncHandler(async (req,res) => {
-  
-  const employee = await Employee.find().select('-__v')
+const listEmployees = asyncHandler(async (req, res) => {
+  const employee = await Employee.find()
+    .select("-__v")
+    .populate({ path: "department", select: "name description" })
+    .populate({ path: "role", select: "name description" });
 
-  if(!employee){
-    throw new ApiError(500,'Employee list not fetched')
+  if (!employee) {
+    throw new ApiError(500, "Employee list not fetched");
   }
 
   const count = employee.length;
 
-  return res.status(200).json(
-    new ApiResponse(200, {count,employee},"Employee list fetched successfully")
-  )
-})
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        { count, employee },
+        "Employee list fetched successfully"
+      )
+    );
+});
 
-const getEmployeeById = asyncHandler( async(req,res) => {
+const getEmployeeById = asyncHandler(async (req, res) => {
+  const { id } = req.params;
 
-  const {id} = req.params;
+  const fetchedEmp = await Employee.findById(id)
+    .select("-__v")
+    .populate({ path: "department", select: "name description" })
+    .populate({ path: "role", select: "name description" });
 
-  const fetchedEmp = await Employee.findById(id).select('-__v');
-
-  if(!fetchedEmp){
-    throw new ApiError(500,'Employee data not fetched')
+  if (!fetchedEmp) {
+    throw new ApiError(500, "Employee data not fetched");
   }
 
   return res
-  .status(200)
-  .json(
-    new ApiResponse(200,fetchedEmp,'Employee data fetched successfully')
-  )
-})
+    .status(200)
+    .json(
+      new ApiResponse(200, fetchedEmp, "Employee data fetched successfully")
+    );
+});
 
-const updateEmployee = asyncHandler(async(req,res) => {
-  const {id} = req.params;
-  const {name,email,phone,salary,department,role} = req.body;
+const updateEmployee = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  const { name, email, phone, salary, department, role } = req.body;
 
   const updatedEmp = await Employee.findByIdAndUpdate(
     id,
     {
-      $set:{
-        name:name.toLowerCase(),
+      $set: {
+        name: name.toLowerCase(),
         email,
         phone,
         salary,
         department,
-        role
-      }
+        role,
+      },
     },
     {
-      new:true
+      new: true,
     }
-  )
+  );
 
-  if(!updatedEmp){
-    throw new ApiError(500,'Can not update employee')
+  if (!updatedEmp) {
+    throw new ApiError(500, "Can not update employee");
   }
 
   return res
-  .status(200)
-  .json(200,updatedEmp,'Employee details updated successfully')
-})
+    .status(200)
+    .json(200, updatedEmp, "Employee details updated successfully");
+});
 
-const deleteEmployee = asyncHandler(async(req,res) => {
-  const {id} = req.params;
+const deleteEmployee = asyncHandler(async (req, res) => {
+  const { id } = req.params;
 
   const deletedEmp = await Employee.findByIdAndDelete(id).select("-__v");
 
-  if(!deletedEmp){
-    throw new ApiError(500,'Can not delete employee')
+  if (!deletedEmp) {
+    throw new ApiError(500, "Can not delete employee");
   }
 
   return res
-  .status(200)
-  .json(
-     new ApiResponse(200,deletedEmp,"Employee deleted successfully")
-  )
-})
+    .status(200)
+    .json(new ApiResponse(200, deletedEmp, "Employee deleted successfully"));
+});
 
-
-
-export { createEmployee,listEmployees,getEmployeeById,updateEmployee,deleteEmployee };
+export {
+  createEmployee,
+  listEmployees,
+  getEmployeeById,
+  updateEmployee,
+  deleteEmployee,
+};
